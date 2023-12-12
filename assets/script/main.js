339ddb1063f0d2ab368a186ddd4c8157708bb6eb
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2023-12-12 13:07:05 +0100

    .

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,20 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+// let interaction = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+// interaction.addEventListener('click', jumpHandler)
+// interaction.addEventListener('animationend', jumpHandler)
 
-function jumpHandler() {
-  interaction.classList.toggle('jump')
+// function jumpHandler() {
+//   interaction.classList.toggle('jump')
+// }
+
+// wheel
+//ik heb hier de wheel getarget om wat mee te doen met de query selector
+var wheel = document.querySelector('.wheel');
+
+//doormiddel van interactie wil ik dat de wiel wat doen in dit geval doormiddel van het klikken 
+wheel.addEventListener('click', spin);
+
+//als de wheel geklikt is wil dat het de aangemaakte css styling gebruikt
+function spin(){
+  wheel.classList.toggle('show-spin');
 }
