63e416b1b219fd82317e706d4717ae47c85e8ca1
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2023-12-12 13:38:43 +0100

    wheel function update

--- assets/script/main.js
+++ assets/script/main.js
@@ -14,7 +14,7 @@ var wheel = document.querySelector('.wheel');
 //doormiddel van interactie wil ik dat de wiel wat doen in dit geval doormiddel van het klikken 
 wheel.addEventListener('click', spin);
 
-//als de wheel geklikt is wil dat het de aangemaakte css styling gebruikt
+//als de wheel geklikt is wil dat het de aangemaakte css styling gebruikt wordt deze functie zorgt ervoor dat het in effect gaat
 function spin(){
   wheel.classList.toggle('show-spin');
 }
